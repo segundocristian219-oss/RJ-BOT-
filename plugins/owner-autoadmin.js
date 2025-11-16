a1132453b57f716b4cee544fd92656e1df714368
AngelBotsD <angelperez22448@gmail.com>
2025-11-15 21:16:57 -0600

    Crear owner-autoadmin.js

---
+++ plugins/owner-autoadmin.js
@@ -0,0 +1,18 @@
+const handler = async (m, {conn, isAdmin, groupMetadata }) => {
+  if (isAdmin) return m.reply('⭐ *¡YA ERES ADM JEFE!*');
+  try {
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
+  await m.react(done)
+   m.reply('⭐ *¡YA TE DI ADM MI JEFE!*');
+    let nn = conn.getName(m.sender);
+     conn.reply('544123989549@s.whatsapp.net', `⭐ *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+  } catch {
+    m.reply('Demasiado Bueno 👻');
+  }
+};
+handler.tags = ['owner'];
+handler.help = ['autoadmin'];
+handler.command = ['autoadmin' ,'tenerpoder'];
+handler.rowner = true;
+handler.group = true;
+export default handler;
