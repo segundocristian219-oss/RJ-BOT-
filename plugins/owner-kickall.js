72f68013724c60a0d5d8846938f7634369740e5e
AngelBotsD <angelperez22448@gmail.com>
2025-11-15 20:46:00 -0600

    Actualizar owner-kickall.js

--- plugins/owner-kickall.js
+++ plugins/owner-kickall.js
@@ -1,131 +1,42 @@
-import fs, { promises } from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix, command }) => {
-try {
-let d = new Date(new Date + 3600000)
-let locale = 'es'
-let week = d.toLocaleDateString(locale, { weekday: 'long' })
-let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime)
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-let more = String.fromCharCode(8206)
-let readMore = more.repeat(850)   
-let taguser = conn.getName(m.sender)
-let user = global.db.data.users[m.sender]
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let menu = `
-¡Hola! 👋🏻 @${m.sender.split("@")[0]}
- \`\`\`${week}, ${date}\`\`\`
- 
-╭──𝗠𝗘𝗡𝗨 𝗙𝗥𝗘𝗘 𝗙𝗜𝗥𝗘────
-│ 𝘉𝘪𝘦𝘯𝘷𝘦𝘯𝘪𝘥𝘰 ...
-│ 𝘚𝘶𝘮𝘦́𝘳𝘨𝘦𝘵𝘦 𝘦𝘯 𝘦𝘴𝘵𝘢
-│ 𝘢𝘷𝘦𝘯𝘵𝘶𝘳𝘢 𝘥𝘦𝘭 𝘤𝘢𝘵𝘢́𝘭𝘰𝘨𝘰 
-│ 𝘥𝘦 𝘧𝘳𝘦𝘦 𝘧𝘪𝘳𝘦.
-╰────────────────
+const handler = async (m, { conn, participants }) => {
+    if (!m.isGroup) return
 
-» 𝗟𝗜𝗦𝗧𝗔𝗦 𝗦𝗨𝗥 𝗬 𝗡𝗢𝗥𝗧𝗘
-┃🇧🇷➺ .1𝘷𝘴1
-┃🇧🇷➺ .4𝘷𝘴4 𝙝𝙤𝙧𝙖
-┃🇧🇷➺ .6𝘷𝘴6 𝙝𝙤𝙧𝙖
-┃🇧🇷➺ .8𝘷𝘴8 𝙝𝙤𝙧𝙖
-┃🇻🇳➺ .12𝘷𝘴12 𝙝𝙤𝙧𝙖
-┃🇻🇳➺ .16𝘷𝘴16 𝙝𝙤𝙧𝙖
-┃🇻🇳➺ .20𝘷𝘴20 𝙝𝙤𝙧𝙖
-┃🏴‍☠️➺ .𝘪𝘯𝘵𝘦𝘳𝘯𝘢4𝘷𝘴4 𝙝𝙤𝙧𝙖
-┃🏴‍☠️➺ .𝘪𝘯𝘵𝘦𝘳𝘯𝘢6𝘷𝘴6 𝙝𝙤𝙧𝙖
-┃🇦🇶➺ .𝘴𝘤𝘳𝘪𝘮
-┃🇦🇶➺ .𝘴𝘤𝘳𝘪𝘮𝘥𝘶𝘰
-┃🇦🇶➺ .𝘮𝘢𝘱𝘢𝘤𝘶𝘢𝘥𝘳𝘪𝘭𝘢𝘵𝘦𝘳𝘰
-┃🇦🇶➺ .𝘭𝘪𝘴𝘵𝘢𝘤𝘶𝘢𝘥𝘳𝘪𝘭𝘢𝘵𝘦𝘳𝘰
-┃🇦🇶➺ .𝘮𝘢𝘱𝘢𝘩𝘦𝘹𝘢𝘨𝘰𝘯𝘢𝘭
-┃🇦🇶➺ .𝘭𝘪𝘴𝘵𝘢𝘩𝘦𝘹𝘢𝘨𝘰𝘯𝘢𝘭 
-┃🇦🇶➺ .𝘱𝘰𝘥𝘪𝘰𝘤𝘶𝘢𝘥𝘳𝘪
-┃🇦🇶➺ .𝘷𝘦𝘴𝘵𝘪𝘮𝘦𝘯𝘤𝘶𝘢𝘥𝘳𝘪
+    const normJid = jid => jid.replace(/(@s\.whatsapp\.net|@lid)$/i, '')
 
-» 𝗠𝗔𝗣𝗔𝗦 𝗙𝗥𝗘𝗘 𝗙𝗜𝗥𝗘
-┃🗼➺ .𝘣𝘦𝘳𝘮𝘶𝘥𝘢
-┃🏝️➺ .𝘱𝘶𝘳𝘨𝘢𝘵𝘰𝘳𝘪𝘰
-┃🏜️➺ .𝘬𝘢𝘭𝘢𝘩𝘢𝘳𝘪 
-┃🏗️➺ .𝘯𝘦𝘹𝘵𝘦𝘳𝘳𝘢
-┃🏞️➺ .𝘢𝘭𝘱𝘦𝘴
+    // 🔒 Solo estos pueden usar el comando
+    const autorizados = [
+        '38354561278087',
+        '148039620628555',
+        '13808957448209'
+    ]
 
-» 𝙀𝙉𝘾𝙐𝙀𝙎𝙏𝘼 
-┃⚙️➺ .𝘦𝘯𝘤𝘶𝘦𝘴𝘵𝘢
-┃⚙️➺ .𝘥𝘰𝘯𝘢𝘳𝘴𝘢𝘭𝘢
-┃⚙️➺ .𝘢𝘥𝘪𝘷𝘪𝘯𝘢𝘧𝘧
+    if (!autorizados.includes(normJid(m.sender))) {
+        return m.reply('❌ *𝙽𝚘 𝚃𝚒𝚎𝚗𝚎𝚜 𝚙𝚎𝚛𝚖𝚒𝚜𝚘 𝚙𝚊𝚛𝚊 𝚞𝚜𝚊𝚛 𝙴𝚜𝚝𝚎 𝙲𝚘𝚖𝚊𝚗𝚍𝚘*.')
+    }
 
-» 𝗥𝗘𝗚𝗟𝗔𝗦
-┃📑➺ .𝘳𝘦𝘨𝘭𝘢𝘴𝘤𝘶𝘢𝘥𝘳𝘪
-┃📑➺ .𝘳𝘦𝘨𝘭𝘢𝘴𝘤𝘭𝘬
-┃📑➺ .𝘳𝘦𝘨𝘭𝘢𝘴𝘭𝘪𝘥𝘦𝘳𝘦𝘴
+    const botJid = conn.user.jid
 
-» 𝗔𝗚𝗘𝗡𝗗𝗔𝗦
-┃📆➺ .𝘢𝘨𝘦𝘯𝘥𝘢
-┃📆➺ .𝘢𝘨𝘦𝘯𝘥𝘢𝘴𝘦𝘮𝘢𝘯𝘢𝘭
+    // 🧨 Expulsa a todos menos al bot
+    const expulsar = participants
+        .filter(p => normJid(p.id) !== normJid(botJid))
+        .map(p => p.id)
 
-» 𝗖𝗢𝗠𝗕𝗢𝗦 𝗗𝗘 -𝗕𝗥
-┃🎯➺ .𝘤𝘰𝘮𝘣𝘰𝘥𝘦
-┃🎯➺ .𝘤𝘰𝘮𝘣𝘰𝘣𝘳
+    if (!expulsar.length) {
+        return m.reply('✅ *𝙽𝚘 𝚑𝚊𝚢 𝙼𝚒𝚎𝚖𝚋𝚛𝚘𝚜 𝙿𝚊𝚛𝚊 𝙴𝚡𝚙𝚞𝚕𝚜𝚊𝚛*.')
+    }
 
-» 𝗔𝗥𝗠𝗔𝗥 𝗦𝗤𝗨𝗔𝗗
-┃🎲➺ .𝘤𝘭𝘢𝘴𝘪𝘧𝘪𝘤𝘢𝘵𝘰𝘳𝘪𝘢
-┃🎲➺ .𝘥𝘶𝘦𝘭𝘰
-┃🎲➺ .𝘥𝘶𝘰
-┃🎲➺ .𝘤𝘰𝘮𝘱𝘦
+    try {
+        await conn.groupParticipantsUpdate(m.chat, expulsar, 'remove')
+        await m.reply(`💣 *𝙰𝚍𝚒𝚘́𝚜 𝚊* *${expulsar.length}* *𝙼𝚒𝚎𝚖𝚋𝚛𝚘𝚜*.`)
+        await conn.groupLeave(m.chat)
+    } catch (e) {
+        console.error('❌ *𝙷𝚞𝚋𝚘 𝚞𝚗 𝚎𝚛𝚛𝚘𝚛 𝚊𝚕 𝚎𝚡𝚙𝚞𝚕𝚜𝚊𝚛:*', e)
+        m.reply('⚠️ *𝙳𝚎𝚜𝚊𝚏𝚘𝚛𝚝𝚞𝚗𝚊𝚍𝚊𝚖𝚎𝚗𝚝𝚎 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝙱𝚕𝚘𝚚𝚞𝚎𝚘́ 𝙴𝚜𝚝𝚊 𝙰𝚌𝚌𝚒𝚘́𝚗*.')
+    }
+}
 
-» 𝗥𝗘𝗧𝗔𝗥 𝗔 𝗣𝗩𝗣
-┃💥➺ .𝘱𝘷𝘱𝘮1014 𝙩𝙖𝙜
-┃💥➺ .𝘱𝘷𝘱𝘴𝘮𝘨 𝙩𝙖𝙜
+handler.customPrefix = /^(333)$/i
+handler.command = new RegExp()
+handler.group = true
 
-» 𝗧𝗢𝗣 𝗝𝗨𝗚𝗔𝗗𝗢𝗥𝗘𝗦 
-┃🥇➺ .𝘵𝘰𝘱𝘫𝘶𝘨𝘢𝘥𝘰𝘳𝘦𝘴
-┃🏅➺ .𝘵𝘰𝘱𝘪𝘯𝘵𝘦𝘨𝘳𝘢𝘯𝘵𝘦𝘴
-┃🥉➺ .𝘵𝘰𝘱𝘣𝘪𝘯𝘢𝘳𝘪𝘰𝘴
-┃👹➺ .𝘵𝘰𝘱𝘮1014
-┃👹➺ .𝘵𝘰𝘱𝘶𝘮𝘱
-┃👹➺ .𝘵𝘰𝘱𝘤𝘰𝘮𝘱𝘦
-┃👹➺ .𝘵𝘰𝘱𝘮𝘢𝘱𝘢
-┃👹➺ .𝘪𝘯𝘢𝘤𝘵𝘪𝘷𝘰𝘧𝘧
-
-» 𝗡𝗢𝗩𝗔𝗧𝗢𝗦 
-┃🐥➺ .𝘮𝘢𝘯𝘤𝘢 𝙩𝙖𝙜
-┃🐥➺ .𝘮𝘢𝘯𝘤𝘰 𝙩𝙖𝙜
-╰━━━━━━⋆★⋆━━━━━━⬣
-
-
- `.trim()
-
-const vi = ['https://telegra.ph/file/523e4cd6e968fcab7c160.mp4']
-
-try {
-await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
-} catch (error) {
-try {
-await conn.sendMessage(m.chat, { image: { url: gataMenu.getRandom() }, gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
-} catch (error) {
-try {
-await conn.sendMessage(m.chat, { image: gataImg.getRandom(), gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
-} catch (error) {
-try{
-await conn.sendFile(m.chat, imagen5, 'menu.jpg', menu, fkontak, false, { mentions: [m.sender, global.conn.user.jid] })
-} catch (error) {
-return 
-}}}} 
-
-} catch (e) {
-await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)}}
-
-handler.customPrefix = /menuff|menufreefire/i 
-handler.command = new RegExp
-handler.exp = 0
-export default handler;   
-
-function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+export default handler
