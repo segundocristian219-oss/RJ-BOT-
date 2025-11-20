c16264e9ed7f73ee87752bbc54e75ca8757aa95a
AngelBotsD <angelperez22448@gmail.com>
2025-11-19 18:58:30 -0600

    Actualizar grupos-tagall.js

--- plugins/grupos-tagall.js
+++ plugins/grupos-tagall.js
@@ -71,8 +71,8 @@ const handler = async (msg, { conn }) => {
     const total = mentionIds.length;
 
     let texto = `*!  MENCION GENERAL  !*\n`;
-    texto += `   *PARA ${total} MIEMBROS* 📣\n\n`;
-    texto += mentionIds.map(id => `┊» 🥀 @${id.split("@")[0]}`).join("\n");
+    texto += `   *PARA ${total} MIEMBROS* 🍄\n\n`;
+    texto += mentionIds.map(id => `┊» 🍄 @${id.split("@")[0]}`).join("\n");
 
     await conn.sendMessage(chatId, { text: texto, mentions: mentionIds }, { quoted: msg });
 
