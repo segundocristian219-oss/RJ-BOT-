988c3df0eb6251ff6a0d02d6144846ac32f10a03
AngelBotsD <angelperez22448@gmail.com>
2025-11-23 11:15:17 -0600

    Actualizar grupos-hidetag.js

--- plugins/grupos-hidetag.js
+++ plugins/grupos-hidetag.js
@@ -2,7 +2,7 @@ import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 let thumb = null
-fetch('https://cdn.russellxz.click/256104e8.jpeg')
+fetch('https://cdn.russellxz.click/21d14f32.jpeg')
   .then(r => r.arrayBuffer())
   .then(buf => thumb = Buffer.from(buf))
   .catch(() => null)
